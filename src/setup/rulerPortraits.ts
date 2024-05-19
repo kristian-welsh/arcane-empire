8239e3172694ce5c72cd5985a523179cd19b168f
JGoodHub <25795840+JGoodHub@users.noreply.github.com>
2024-05-19 14:57:15 +0100

    Improved portraits imports and defined a couple more prettier rules

--- src/setup/rulerPortraits.ts
+++ src/setup/rulerPortraits.ts
@@ -1,32 +1,9 @@
-import portrait_0 from '../assets/ui/ruler_portraits/portrait_0.png';
-import portrait_1 from '../assets/ui/ruler_portraits/portrait_1.png';
-import portrait_2 from '../assets/ui/ruler_portraits/portrait_2.png';
-import portrait_3 from '../assets/ui/ruler_portraits/portrait_3.png';
-import portrait_4 from '../assets/ui/ruler_portraits/portrait_4.png';
-import portrait_5 from '../assets/ui/ruler_portraits/portrait_5.png';
-import portrait_6 from '../assets/ui/ruler_portraits/portrait_6.png';
-import portrait_7 from '../assets/ui/ruler_portraits/portrait_7.png';
-import portrait_8 from '../assets/ui/ruler_portraits/portrait_8.png';
-import portrait_9 from '../assets/ui/ruler_portraits/portrait_9.png';
-import portrait_10 from '../assets/ui/ruler_portraits/portrait_10.png';
-import portrait_11 from '../assets/ui/ruler_portraits/portrait_11.png';
-import portrait_12 from '../assets/ui/ruler_portraits/portrait_12.png';
-
-export const portraitSources: string[] = [
-  portrait_0,
-  portrait_1,
-  portrait_2,
-  portrait_3,
-  portrait_4,
-  portrait_5,
-  portrait_6,
-  portrait_7,
-  portrait_8,
-  portrait_9,
-  portrait_10,
-  portrait_11,
-  portrait_12,
-];
+export const portraitSources: string[] = [];
+for (let i = 0; i <= 12; i++) {
+  import(`../assets/ui/ruler_portraits/portrait_${i}.png`).then((result) => {
+    portraitSources.push(result.default);
+  });
+}
 
 export const getSeededPortraitForName = (name: string): string => {
   let hash = 0;
